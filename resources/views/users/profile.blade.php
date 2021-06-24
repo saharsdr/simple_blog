c5972f2d5e56d773481eed17c1363193540c5a82
Sahar <sahar.sadrii.ss@gmail.com>
2021-06-24 13:51:41 +0430

    profile page Done.

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -21,27 +21,42 @@
                     <div class="row g-2">
                         <div class="col-md">
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="name" placeholder="سحر" value="سحر" >
+                            <input type="text" class="form-control" id="name" placeholder="سحر" value="@php echo auth()->user()->name_first; @endphp" >
                             <label for="name">نام</label>
                         </div>
                         </div>
                         <div class="col-md">
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="last_name" placeholder="صدری" value="صدری">
+                            <input type="text" class="form-control" id="last_name" placeholder="صدری" value="@php echo auth()->user()->name_last; @endphp">
                             <label for="name">نام خانوادگی</label>
                         </div>
                     </div>
                     </div>
                     <div class="form-floating my-2">
-                        <input type="email" class="form-control" id="email" value="sahar.sadrii.ss@gmail.com" placeholder="name@example.com">
+                        <input type="email" class="form-control" id="email" value="@php echo auth()->user()->email; @endphp" placeholder="name@example.com">
                         <label for="email">ایمیل</label>
                     </div>
                     <div class="form-floating my-2">
-                        <input type="text" class="form-control" id="student_number" value="961845125" placeholder="شماره دانشجویی">
+                        <input type="text" class="form-control" id="student_number" value="@php echo auth()->user()->student_number; @endphp" placeholder="شماره دانشجویی">
                         <label for="student_number">شماره دانشجویی</label>
                     </div>
                 </fieldset>
-                <p class="fs-6 rounded-pill bg-warning d-inline p-2 my-4" style="margin-top: 2rem;">نوع کاربر : <span>عادی فعال</span></p>
+                <p class="fs-6 rounded-pill bg-warning d-inline p-2 my-4" style="margin-top: 2rem;">نوع کاربر : <span>
+                    @if (auth()->user()->type == 3)
+                        عادی
+                    @else 
+                        @if (auth()->user()->type == 2)
+                         نویسنده
+                        @else
+                            ادمین
+                        @endif
+                    @endif
+                    @if (auth()->user()->is_active)
+                         فعال
+                    @else
+                         غیر فعال
+                    @endif
+                    </span></p>
             </form>
         </div>
     </div>
